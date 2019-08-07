73ca43fa54a75d33e4894791ed8dbee0c110106e
toyo <709292639@qq.com>
2019-08-07 13:14:41 +0800

    node

--- routes/shopping.js
+++ routes/shopping.js
@@ -20,6 +20,28 @@ router.use('/wares', (request, response, next) => {
 })
 
 
+// 获取购物车
+router.use('/usershop', (request, response, next) => {
+    let sql = "SELECT * FROM `userinfo` WHERE uname=?";
+    let params = [request.body.uname];
+    db.query(sql, params).then(data => {
+        console.info()
+        response.json(data[0])
+    }, err => {
+        response.json(err);
+    })
+})
+// 添加购物车接口
+router.use('/addshop', (request, response, next) => {
+    let addObj = request.body;
+    let sql = "UPDATE `userinfo` SET ushop=? WHERE uname=?";
+    let params = [addObj.ushop, addObj.uname];
+    db.query(sql, params).then(res => {
+        response.json(res);
+    });
+})
+
+
 module.exports = {
     router
 }
