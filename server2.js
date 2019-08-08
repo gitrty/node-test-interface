c0469ed98b35127e907a2d3298d7affbd07ae1e8
toyo <709292639@qq.com>
2019-08-08 09:45:19 +0800

    nide

--- server2.js
+++ server2.js
@@ -22,15 +22,15 @@ app.listen(8088, () => {
 
 // 拦截器
 app.use('*', (request, response, next) => {
-    // if (   //   配置多个 允许跨域的域名
-    //     request.headers.origin == 'http://127.0.0.1:8080' ||
-    //     request.headers.origin == 'https://www.baidu.com'
-    // ) {
+    if (   //   配置多个 允许跨域的域名
+        request.headers.origin == 'http://127.0.0.1:8080' ||
+        request.headers.origin == 'http://localhost:8080'
+    ) {
     // 设置返回值编码
     response.setHeader("Content-Type", "application/json;charset=utf-8");
     //设置允许跨域的域名，*代表允许任意域名跨域  
     // ***********获取服务器session时 =>Access-Control-Allow-Origin不能设为星号，必须指定明确的，与请求网页一致的域名*************
-    response.header("Access-Control-Allow-Origin", 'http://127.0.0.1:8080');
+    response.header("Access-Control-Allow-Origin", request.headers.origin);
     //允许的header类型
     response.header("Access-Control-Allow-Headers", 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     //跨域允许的请求方式 
@@ -38,7 +38,7 @@ app.use('*', (request, response, next) => {
 
     response.header('Access-Control-Allow-Credentials', true);
     next();
-    // }
+    }
 });
 
 app.use('/api', require('./routes/api').router);
